b65464d815ee8307bc2985263d153140f9a8457e
grig-ar <a.grigorovich@g.nsu.ru>
2020-09-12 13:59:04 +0700

    Add AppExecutor class

---
+++ src/main/java/db/executor/AppExecutor.java
@@ -0,0 +1,20 @@
+package db.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class AppExecutor {
+    private final ExecutorService diskIO;
+
+    private AppExecutor(ExecutorService diskIO) {
+        this.diskIO = diskIO;
+    }
+
+    AppExecutor() {
+        this(Executors.newSingleThreadExecutor());
+    }
+
+    public ExecutorService diskIO() {
+        return diskIO;
+    }
+}
