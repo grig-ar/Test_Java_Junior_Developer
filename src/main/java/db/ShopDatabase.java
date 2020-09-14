debb06d14725a507c8bfbba00630d48444b1a589
grig-ar <a.grigorovich@g.nsu.ru>
2020-09-14 13:17:02 +0700

    Implemented ShopDatabase

--- src/main/java/db/ShopDatabase.java
+++ src/main/java/db/ShopDatabase.java
@@ -2,7 +2,12 @@ package db;
 
 import db.executor.AppExecutor;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ShopDatabase {
+    private DataSource dataSource;
     private static volatile ShopDatabase instance;
     private AppExecutor appExecutor;
 
@@ -10,23 +15,32 @@ public class ShopDatabase {
 
     }
 
-    public static ShopDatabase getInstance(AppExecutor executor) {
+    public static ShopDatabase getInstance(AppExecutor executor, DataSource dataSource) {
         if (instance == null) {
             synchronized (ShopDatabase.class) {
                 if (instance == null) {
                     instance = new ShopDatabase();
                     instance.setExecutor(executor);
+                    instance.setDataSource(dataSource);
                 }
             }
         }
         return instance;
     }
 
-    private void setExecutor(AppExecutor executors) {
-        appExecutor = executors;
+    private void setExecutor(AppExecutor executor) {
+        appExecutor = executor;
+    }
+
+    private void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public AppExecutor getAppExecutor() {
         return appExecutor;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 }
--- src/main/java/db/executor/AppExecutor.java
+++ src/main/java/db/executor/AppExecutor.java
@@ -10,7 +10,7 @@ public class AppExecutor {
         this.diskIO = diskIO;
     }
 
-    AppExecutor() {
+    public AppExecutor() {
         this(Executors.newSingleThreadExecutor());
     }
 
