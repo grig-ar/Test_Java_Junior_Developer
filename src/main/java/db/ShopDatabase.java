34092c2bcf61baa14020f2cda1abaa46f04cd9c6
grig-ar <a.grigorovich@g.nsu.ru>
2020-09-17 15:54:54 +0700

    Update ShopDatabase

--- src/main/java/db/ShopDatabase.java
+++ src/main/java/db/ShopDatabase.java
@@ -1,72 +1,41 @@
 package db;
 
-import db.dao.ShopCustomerDao;
-import db.dao.ShopProductDao;
-import db.dao.ShopPurchaseDao;
-import db.executor.AppExecutor;
-
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ShopDatabase {
-    private DataSource dataSource;
+    private static DataSource dataSource;
     private static volatile ShopDatabase instance;
-    private AppExecutor appExecutor;
-
-    private ShopPurchaseDao purchaseDao;
-    private ShopProductDao productDao;
-    private ShopCustomerDao customerDao;
 
     private ShopDatabase() {
 
     }
 
-    public static ShopDatabase getInstance(AppExecutor executor, DataSource dataSource) {
+    public static ShopDatabase getInstance(DataSource dataSource) {
         if (instance == null) {
             synchronized (ShopDatabase.class) {
                 if (instance == null) {
                     instance = new ShopDatabase();
-                    instance.setExecutor(executor);
-                    instance.setDataSource(dataSource);
-                    instance.setDao();
+                    ShopDatabase.dataSource = dataSource;
                 }
             }
         }
         return instance;
     }
 
-    private void setExecutor(AppExecutor executor) {
-        appExecutor = executor;
+    public DataSource getDataSource() {
+        return dataSource;
     }
 
-    private void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public static <T> T execute(Callback<Connection, T> body) throws SQLException {
+        try (Connection conn = dataSource.getConnection()) {
+            return body.apply(conn);
+        }
     }
 
-    private void setDao() {
-        this.customerDao = new ShopCustomerDao(this);
-        this.productDao= new ShopProductDao(this);
-        this.purchaseDao = new ShopPurchaseDao(this);
+    public interface Callback<T, R> {
+        R apply(T t) throws SQLException;
     }
 
-    public ShopPurchaseDao getPurchaseDao() {
-        return purchaseDao;
-    }
-
-    public ShopProductDao getProductDao() {
-        return productDao;
-    }
-
-    public ShopCustomerDao getCustomerDao() {
-        return customerDao;
-    }
-
-    public AppExecutor getAppExecutor() {
-        return appExecutor;
-    }
-
-    public Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
-    }
 }
